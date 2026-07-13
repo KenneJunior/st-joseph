5bc5f2d0eea6a735f8d67c9bdf14d1719f6c9a68
Kenne junior <fouomenekennebonifacejunior@gmail.com>
2026-07-14 00:14:14 +0100

    edited the vite.config.ts to include the prospectus page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: './',
     build: {
+        rolldownOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                prospectus: resolve(__dirname, 'prospectus.html')
+            }
+        },
         outDir: 'dist',
         emptyOutDir: true,
         target: 'esnext',
